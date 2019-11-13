5d6578b45c22860307ae7ba8e0c04d2f61c7fdd9
zaratustrawilliam <willivasquez98@gmail.com>
2019-11-13 18:54:38 -0500

    sin prol

---
+++ SemilleroHBTSegundaEdicion/ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/pojo/EjerciciosPOJO.java
@@ -0,0 +1,14 @@
+/**
+ * EjerciciosPOJO.java
+ */
+package com.hbt.semillero.pojo;
+
+/**
+ * <b>Descripción:<b> Clase que determina
+ * <b>Caso de Uso:<b> 
+ * @author acer
+ * @version 
+ */
+public class EjerciciosPOJO {
+
+}
---
+++ SemilleroHBTSegundaEdicion/ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/EjeciciosTest.java
@@ -0,0 +1,14 @@
+/**
+ * EjeciciosTest.java
+ */
+package com.hbt.semillero.servicios;
+
+/**
+ * <b>Descripción:<b> Clase que determina
+ * <b>Caso de Uso:<b> 
+ * @author acer
+ * @version 
+ */
+public class EjeciciosTest {
+
+}
